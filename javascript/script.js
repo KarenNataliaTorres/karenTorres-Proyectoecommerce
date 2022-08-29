b216ed0f3e6568982d1c03836d5160e368f2b567
KarenNataliaTorres <karen.torres.ns@gmail.com>
2022-08-29 16:40:59 -0300

    simulador interactivo

--- javascript/script.js
+++ javascript/script.js
@@ -14,6 +14,9 @@ while(aniadirMasProductos===true){
         totalCompra = totalCompra+2000
     }else if(productoSeleccionado === 4){
         totalCompra = totalCompra+250
+    }else{
+        productoSeleccionado=parseInt(prompt(`Ingresar producto válido 1.Pantalones - 2.Chaquetas - 3.Zapatos - 4.Accesorio`))
+        continue
     }
 
 decidir = parseInt(prompt(`Desea continuar comprando? 1.SI 2.NO`))
@@ -28,3 +31,20 @@ if(decidir===1){
 }
 }
 alert(`el valor total de la compra es: `+ totalCompra)
+
+function calcularPrecioConCupon(valor){
+    let descuento = 0
+    let cupon = parseInt(prompt(`Ingresar cupón para acceder a un descuento: `))
+    if(cupon===100005){
+        descuento = 5
+    }else if(cupon===100010){
+        descuento = 10
+    }else{
+        descuento = 0
+    }
+    let valorDescuento = valor*(descuento/100)
+    valor=valor-valorDescuento
+    return valor
+}
+let valorConDescuento= calcularPrecioConCupon(totalCompra)
+alert(`El costo final de su compra es: `+ valorConDescuento)
